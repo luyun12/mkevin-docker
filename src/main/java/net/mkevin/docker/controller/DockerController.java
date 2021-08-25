7852009f3eddb90f8d939b11ec263e6f2a4a6edc
luyun12 <1432384563@qq.com>
2021-08-25 11:08:57 +0800

    212

--- src/main/java/net/mkevin/docker/controller/DockerController.java
+++ src/main/java/net/mkevin/docker/controller/DockerController.java
@@ -15,7 +15,7 @@ public class DockerController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello Docker431043331!";
+        return "hello Docker4310433314!";
     }
 
  /*   @RequestMapping("/write")
